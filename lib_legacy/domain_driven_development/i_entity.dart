1688ecbc621aa9542a0626de9dfd53474005f926
Rob Campbell <bigbenchrob@gmail.com>
2025-09-22 16:32:51 -0700

    moved old essentials and features into lib_legacy. prepare for DDD migration plan

---
+++ lib_legacy/domain_driven_development/i_entity.dart
@@ -0,0 +1,5 @@
+// import './value_objects.dart';
+
+// abstract class IEntity {
+//   UniqueIntId get id;
+// }
---
+++ lib_legacy/features/chats/presentation/widgets/chats_main_panel.dart
@@ -0,0 +1,22 @@
+// import 'package:flutter/material.dart';
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// import '../../../../../lib/features/contacts/feature_level_providers.dart';
+
+// class ChatsMainPanel extends ConsumerWidget {
+//   const ChatsMainPanel({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final chosenId = ref.watch(chosenContactIdProvider);
+//     final text = chosenId == -1
+//         ? 'No contact chosen'
+//         : 'Contact $chosenId chats:';
+//     return Center(
+//       child: Text(
+//         text,
+//         style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+//       ),
+//     );
+//   }
+// }
