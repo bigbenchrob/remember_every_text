3ace2060f6cb590d8f582eaeb977c8a2aa5902d2
Rob Campbell <bigbenchrob@gmail.com>
2026-05-16 09:10:08 -0700

    Add handle stage report

---
+++ lib/essentials/incremental_update/application/handles/orchestrators/handle_stage_report.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+
+import '../../../domain/models/handle_snapshot_delta.dart';
+import '../../../domain/sealed_unions/handle_import_decision.dart';
+import '../../../domain/sealed_unions/handle_sync_state.dart';
+import '../importers/handle_importer.dart';
+
+enum HandleStageExecutionOutcome { skipped, blocked, executed }
+
+@immutable
+final class HandleStageReport {
+  HandleStageReport({
+    required this.startedAt,
+    required this.finishedAt,
+    required this.preExecutionDelta,
+    required this.preExecutionState,
+    required this.decision,
+    required this.executionOutcome,
+    this.importResult,
+    this.postExecutionDelta,
+    this.postExecutionState,
+    List<String> diagnosticEvents = const <String>[],
+  }) : diagnosticEvents = List.unmodifiable(diagnosticEvents);
+
+  final DateTime startedAt;
+  final DateTime finishedAt;
+  final HandleSnapshotDelta preExecutionDelta;
+  final HandleSyncState preExecutionState;
+  final HandleImportDecision decision;
+  final HandleStageExecutionOutcome executionOutcome;
+  final HandleImportResult? importResult;
+  final HandleSnapshotDelta? postExecutionDelta;
+  final HandleSyncState? postExecutionState;
+  final List<String> diagnosticEvents;
+}
