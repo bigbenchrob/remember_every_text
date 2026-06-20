e5139dc2052fbe56682dcaff6f6695c985395d7e
Rob Campbell <bigbenchrob@gmail.com>
2026-06-20 04:44:04 -0700

    Test conversation favourite action boundary

---
+++ test/essentials/conversation_graph/application/conversation_favourites/conversation_favourite_actions_provider_test.dart
@@ -0,0 +1,57 @@
+import 'package:drift/native.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:remember_this_text/essentials/conversation_graph/application/conversation_favourites/conversation_favourite_actions_provider.dart';
+import 'package:remember_this_text/essentials/conversation_graph/application/conversation_favourites/conversation_favourites_provider.dart';
+import 'package:remember_this_text/essentials/db/feature_level_providers.dart';
+import 'package:remember_this_text/essentials/db/infrastructure/data_sources/local/overlay/overlay_database.dart';
+
+void main() {
+  group('ConversationFavouriteActions', () {
+    late OverlayDatabase overlayDb;
+
+    ProviderContainer buildContainer() {
+      return ProviderContainer(
+        overrides: [
+          overlayDatabaseProvider.overrideWith((ref) async => overlayDb),
+        ],
+      );
+    }
+
+    setUp(() {
+      overlayDb = OverlayDatabase(NativeDatabase.memory());
+    });
+
+    tearDown(() async {
+      await overlayDb.close();
+    });
+
+    test('toggles Core favourites through the action boundary', () async {
+      final container = buildContainer();
+      addTearDown(container.dispose);
+
+      await container
+          .read(conversationFavouriteActionsProvider.notifier)
+          .toggleCoreFavourite(8796093022216);
+
+      expect(
+        container
+            .read(conversationFavouritesControllerProvider)
+            .coreConversationIds,
+        [8796093022216],
+      );
+
+      await container
+          .read(conversationFavouriteActionsProvider.notifier)
+          .toggleCoreFavourite(8796093022216);
+
+      expect(
+        container
+            .read(conversationFavouritesControllerProvider)
+            .coreConversationIds,
+        isEmpty,
+      );
+    });
+  });
+}
